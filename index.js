707385b76057366fd4326caaa5331ec4feb0957e
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-05 23:00:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,20 +21,11 @@ function inc(a){
 function dec (a){
   return (--a)
 }
+
 function makeInt(n){
   return(parseInt(n))
 }
 
-
-/*
- 
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
-*/
+function preserveDecimal(n){
+  return(parseFloat(n))
+}
